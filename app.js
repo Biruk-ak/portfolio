e4c622afaffc571850d92c52d20f46f8e8fd733b
biruk aklillu <birukaklilu0110@gmail.com>
2023-05-25 01:07:54 +0300

    porfolio

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 const reveals = document.querySelectorAll('.reveals')
+const about = document.getElementById('about')
+const about_me_section = document.getElementById('about_me_section')
+const lists = document.querySelectorAll('.lists')
 
-window.addEventListener('scroll', function(){
+// about.addEventListener('click', function(){
+//     const sectionposition = about_me_section.offsetTop;
+//     Window.scrollTo({
+//         top: sectionposition,
+//         behavior: 'smooth',
+//         block: "start"
 
-    for(let i = o; i<reveals.length ; i++){
-        let windowhight = window.innerHeight;
-        let revealtop = reveals[i].getBoundingClientRect().top;
-        let revealpoint = 150;
-        if(revealtop < windowhight - revealpoint){
-            reveals[i].classList.add('active')
-        }
-        else{
-            reveals[i].classList.remove('active')
-        }
-    }
+//     });
+// })
 
+
+
+lists.forEach((item) => {
+    
+    item.addEventListener('click', () =>{
+        
+        // let el = document.getElementById()
+        // e.getAttribute("data-link")
+        console.log(item.getAttribute("data-link"));
+        // el.scrollIntoView({
+        //     behavior: 'smooth',
+        //     block: 'start'
+        // })
+       
+    })
+   
+})
+
+// window.addEventListener('scroll',reveal()) 
+
+// function reveal(){
+//     // alert("");
+//     for(let i = 0; i<reveals.length ; i++){
+//         let windowhight = window.innerHeight;
+//         let revealtop = reveals[i].getBoundingClientRect().top;
+//         let revealpoint = 200;
+//         if(revealtop < windowhight - revealpoint){
+//             reveals[i].classList.add('active')
+//         }
+//         else{
+//             reveals[i].classList.remove('active')
+//         }
+//     }
+// };
+
+var typed = new Typed(".text-type",{
+    strings: ["Web Developer","Designer","Programmer"],
+    typeSpeed: 300,
+    backSpeed: 200,
+    loop : true
 })
